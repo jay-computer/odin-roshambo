b14409a1dfb9027b812dd795de339197652a8e91
jay <joshuakang19@gmail.com>
2022-06-22 11:42:58 -0400

    Finish first iteration

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
@@ -25,6 +26,7 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+    /*
     let results = new Array(5);
     let playerWins = 0;
     let computerWins = 0;
@@ -56,6 +58,58 @@ function game() {
         console.log("computerWins: " + computerWins);
         return "You and the computer tied."
     }
+    */
+    
+}
+const batton = document.querySelectorAll(".on");
+let results = new Array(5);
+let playerWins = 0;
+let computerWins = 0;
+let iterations = 0;
+
+function removeClass(e) {
+    e.target.classList.remove("on");
 }
 
-console.log(game());
+function runGame(e) {
+    if(iterations === 5) {
+        if(playerWins > 2) {
+            console.log("Game over, you won.");
+            return;
+        }
+        if (computerWins > 2) {
+            console.log("Game over, computer won.");
+            return;
+        }
+        if (playerWins == computerWins) {
+            console.log("Game over, you and computer tied.");
+            return;
+        }    
+        
+        
+    }
+    
+    let content = this.textContent || this.innerText;
+    content = content.toLowerCase();
+    
+    let result = playRound(content, computerPlay());
+    console.log(result);
+    
+    if (result.includes("won")) {
+        playerWins = playerWins + 1;
+        
+    }
+    if (result.includes("lost")) {
+        computerWins = computerWins + 1;
+    }
+    iterations = iterations + 1;
+}
+
+
+
+
+//batton.forEach(button => button.addEventListener('keydown', playRound(playerSelection, computerSelection)))
+batton.forEach(button => button.addEventListener("click", runGame));
+
+
+
